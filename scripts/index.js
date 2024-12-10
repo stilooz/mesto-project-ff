1bdae3163d2772199232abbc64d19303879acf19
stilooz <stilooz@yandex.ru>
2024-12-10 18:54:04 +0300

    second revision вынес функцию удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,33 @@
 const placesList = document.querySelector(".places__list");
 
+const deleteCardHandler = (card) => {
+    const deleteButtonCard = card.querySelector(".card__delete-button");
+    deleteButtonCard.addEventListener(
+      "click",
+      () => {
+        card.remove();
+      },
+      { once: true }
+    );
+  };
+
 const addCard = (name, link) => {
   const cardTemplate = document.querySelector("#card-template").content;
   const card = cardTemplate.querySelector(".card").cloneNode(true);
-  const deleteButtonCard = card.querySelector(".card__delete-button");
+  deleteCardHandler(card);
+//   const deleteButtonCard = card.querySelector(".card__delete-button");
 
   card.querySelector(".card__title").textContent = name;
   card.querySelector(".card__image").setAttribute("src", link);
   card.querySelector(".card__image").setAttribute("alt", `фотография ${name}`);
 
-  deleteButtonCard.addEventListener(
-    "click",
-    () => {
-      card.remove();
-    },
-    { once: true }
-  );
+//   deleteButtonCard.addEventListener(
+//     "click",
+//     () => {
+//       card.remove();
+//     },
+//     { once: true }
+//   );
 
   return card;
 };
