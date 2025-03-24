08cd8724d8f45634934e88b615fe0a497a909e7a
stilooz <stilooz@yandex.ru>
2025-03-24 12:05:08 +0300

    добавил импорт имени и описания, добавил закрытие попапа по трем ивентам esc, click, close

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,21 @@
 import '../pages/index.css';
 import { initialCards } from './cards.js';
 
+// переменные карточек
 const placesList = document.querySelector(".places__list");
+// работа с текстом внутри попапа карандаш
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const nameInput = document.querySelector('.popup__input_type_name');
+const descriptionInput = document.querySelector('.popup__input_type_description');
+const formEditProfile = document.forms['edit-profile']
+// переменные открытия попапа
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupCloseButton = document.querySelector('.popup__close')
+const popups = document.querySelectorAll('.popup')
 
+// удаление карточек
 const deleteCardHandler = (card) => {
   const deleteButtonCard = card.querySelector(".card__delete-button");
   deleteButtonCard.addEventListener(
@@ -29,3 +42,40 @@ const addCard = (name, link) => {
 initialCards.forEach((card) =>
   placesList.append(addCard(card.name, card.link))
 );
+
+// открытие и закрытие попапа
+function popupOpen(popup) {
+  popup.classList.add('popup_is-opened');
+}
+
+function closeOpenedPopup() {
+  const openedPopup = document.querySelector('.popup_is-opened');
+  if (openedPopup) {
+    openedPopup.classList.remove('popup_is-opened');
+  }
+}
+
+profileEditButton.addEventListener('click', () => {
+  // заполнение текстовых полей попапа
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+  popupOpen(popupTypeEdit);
+});
+
+popupCloseButton.addEventListener('click', closeOpenedPopup);
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closeOpenedPopup();
+  }
+});
+
+popups.forEach((popup) => {
+  popup.addEventListener('click', (event) => {
+    if (event.target === popup) {
+      popup.classList.remove('popup_is-opened');
+    }
+  });
+});
+
+// текст в попапе карандаш
