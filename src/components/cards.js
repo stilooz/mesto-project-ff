7a1591f25e149fc59d7b152c01f81138c776e81c
stilooz <stilooz@yandex.ru>
2025-03-25 22:05:31 +0300

    перенес добавление карточки и удаление в card.js

--- src/components/cards.js
+++ src/components/cards.js
@@ -24,3 +24,26 @@ export const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   }
 ];
+
+const deleteCardHandler = (card) => {
+  const deleteButtonCard = card.querySelector(".card__delete-button");
+  deleteButtonCard.addEventListener(
+    "click",
+    () => {
+      card.remove();
+    },
+    { once: true }
+  );
+};
+
+export const createCard = (name, link) => {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  deleteCardHandler(card);
+
+  card.querySelector(".card__title").textContent = name;
+  card.querySelector(".card__image").setAttribute("src", link);
+  card.querySelector(".card__image").setAttribute("alt", `фотография ${name}`);
+
+  return card;
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,16 @@
 import '../pages/index.css';
-import { initialCards } from './cards.js';
+import { initialCards, createCard } from '../components/cards.js';
 
 // переменные карточек
 const placesList = document.querySelector(".places__list");
+const formAddCard = document.forms['new-place'];
+const cardNameInput = document.querySelector('.popup__input_type_card-name');
+const cardLinkInput = document.querySelector('.popup__input_type_url');
 // работа с текстом внутри попапа карандаш
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 const nameInput = document.querySelector('.popup__input_type_name');
-const descriptionInput = document.querySelector('.popup__input_type_description');
+const jobInput = document.querySelector('.popup__input_type_description');
 const formEditProfile = document.forms['edit-profile']
 // переменные открытия попапа
 const profileEditButton = document.querySelector('.profile__edit-button');
@@ -15,32 +18,8 @@ const popupTypeEdit = document.querySelector('.popup_type_edit');
 const popupCloseButton = document.querySelector('.popup__close')
 const popups = document.querySelectorAll('.popup')
 
-// удаление карточек
-const deleteCardHandler = (card) => {
-  const deleteButtonCard = card.querySelector(".card__delete-button");
-  deleteButtonCard.addEventListener(
-    "click",
-    () => {
-      card.remove();
-    },
-    { once: true }
-  );
-};
-
-const addCard = (name, link) => {
-  const cardTemplate = document.querySelector("#card-template").content;
-  const card = cardTemplate.querySelector(".card").cloneNode(true);
-  deleteCardHandler(card);
-
-  card.querySelector(".card__title").textContent = name;
-  card.querySelector(".card__image").setAttribute("src", link);
-  card.querySelector(".card__image").setAttribute("alt", `фотография ${name}`);
-
-  return card;
-};
-
 initialCards.forEach((card) =>
-  placesList.append(addCard(card.name, card.link))
+  placesList.append(createCard(card.name, card.link))
 );
 
 // открытие и закрытие попапа
@@ -58,7 +37,7 @@ function closeOpenedPopup() {
 profileEditButton.addEventListener('click', () => {
   // заполнение текстовых полей попапа
   nameInput.value = profileTitle.textContent;
-  descriptionInput.value = profileDescription.textContent;
+  jobInput.value = profileDescription.textContent;
   popupOpen(popupTypeEdit);
 });
 
@@ -79,3 +58,11 @@ popups.forEach((popup) => {
 });
 
 // текст в попапе карандаш
+formEditProfile.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+
+  closeOpenedPopup();
+});
