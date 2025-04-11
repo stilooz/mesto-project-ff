ac44c84b1681a88aad2430844a3c9bc3efa0aee6
stilooz <stilooz@yandex.ru>
2025-04-11 14:11:36 +0300

    добавил функции показа и удаления ошибки из формы

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -79,3 +79,17 @@ formEditProfile.addEventListener('submit', (evt) => {
   closeModal();
 });
 
+//ошибка
+function showInputError(formElement, inputElement, errorMessage, config) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(config.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(config.errorClass);
+}
+
+function hideInputError(formElement, inputElement, config) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(config.inputErrorClass);
+  errorElement.textContent = '';
+  errorElement.classList.remove(config.errorClass);
+}
