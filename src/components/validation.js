09461114369629e5f11b6ef1a357179ff81e80af
stilooz <stilooz@yandex.ru>
2025-04-14 19:54:45 +0300

    переместил всю валидацию в отдельный файл

---
+++ src/components/validation.js
@@ -0,0 +1,68 @@
+export function showInputError(formElement, inputElement, errorMessage, config) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  if (!errorElement) return;
+  inputElement.classList.add(config.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(config.errorClass);
+};
+
+export function hideInputError(formElement, inputElement, config) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  if (!errorElement) return;
+  inputElement.classList.remove(config.inputErrorClass);
+  errorElement.textContent = '';
+  errorElement.classList.remove(config.errorClass);
+};
+
+export function isValid(formElement, inputElement, config) {
+  const errorMessage = inputElement.dataset.errorMessage;
+
+  if (inputElement.validity.valueMissing && errorMessage) {
+    inputElement.setCustomValidity(errorMessage);
+  } else {
+    inputElement.setCustomValidity('');
+  }
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, config);
+  } else {
+    hideInputError(formElement, inputElement, config);
+  }
+};
+
+export function toggleButtonState(inputList, buttonElement, config) {
+  const hasInvalidInput = inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+  if (hasInvalidInput) {
+    buttonElement.classList.add(config.inactiveButtonClass);
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove(config.inactiveButtonClass);
+    buttonElement.disabled = false;
+  }
+};
+
+function setEventListeners(formElement, config) {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+  toggleButtonState(inputList, buttonElement, config);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement, config);
+      toggleButtonState(inputList, buttonElement, config);
+    });
+  });
+};
+
+export function enableValidation(config) {
+  const formList = Array.from(document.querySelectorAll(config.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement, config);
+  });
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,13 @@
 import '../pages/index.css';
 import { initialCards, createCard } from '../components/cards.js';
 import { openModal, closeModal } from '../components/modal.js';
-
+import {
+  enableValidation,
+  isValid,
+  toggleButtonState,
+  showInputError,
+  hideInputError
+} from '../components/validation.js';
 // переменные карточек
 const placesList = document.querySelector(".places__list");
 const formAddCard = document.forms['new-place'];
@@ -108,78 +114,6 @@ formEditProfile.addEventListener('submit', (evt) => {
 
   closeModal();
 });
-
-//ошибка
-function showInputError(formElement, inputElement, errorMessage, config) {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  if (!errorElement) return;
-  inputElement.classList.add(config.inputErrorClass);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add(config.errorClass);
-};
-
-function hideInputError(formElement, inputElement, config) {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  if (!errorElement) return;
-  inputElement.classList.remove(config.inputErrorClass);
-  errorElement.textContent = '';
-  errorElement.classList.remove(config.errorClass);
-};
-
-//проверка валидности введенного текста
-function isValid(formElement, inputElement, config) {
-  const errorMessage = inputElement.dataset.errorMessage;
-
-  if (inputElement.validity.valueMissing && errorMessage) {
-    inputElement.setCustomValidity(errorMessage);
-  } else {
-    inputElement.setCustomValidity('');
-  }
-
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, config);
-  } else {
-    hideInputError(formElement, inputElement, config);
-  }
-};
-
-//отключение кнопки
-function toggleButtonState(inputList, buttonElement, config) {
-  const hasInvalidInput = inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-  if (hasInvalidInput) {
-    buttonElement.classList.add(config.inactiveButtonClass);
-    buttonElement.disabled = true;
-  } else {
-    buttonElement.classList.remove(config.inactiveButtonClass);
-    buttonElement.disabled = false;
-  }
-};
-
-function setEventListeners(formElement, config) {
-  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-  const buttonElement = formElement.querySelector(config.submitButtonSelector);
-
-  toggleButtonState(inputList, buttonElement, config);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement, config);
-      toggleButtonState(inputList, buttonElement, config);
-    });
-  });
-};
-
-function enableValidation(config) {
-  const formList = Array.from(document.querySelectorAll(config.formSelector));
-
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-    });
-    setEventListeners(formElement, config);
-  });
-};
 
 enableValidation({
   formSelector: '.popup__form',
