2484b97f2f34beb3c8512aa8410addcc243a5126
stilooz <stilooz@yandex.ru>
2025-04-18 12:33:53 +0300

    вынес авторизацию в переменную

--- src/components/api.js
+++ src/components/api.js
@@ -1,3 +1,11 @@
+const config = {
+  baseUrl: 'https://mesto.nomoreparties.co/v1/wff-cohort-36',
+  headers: {
+    authorization: '1b90838c-6394-4588-b4b1-865db972e903',
+    'Content-Type': 'application/json'
+  }
+};
+
 function checkResponse(res) {
   if (res.ok) {
     return res.json();
@@ -5,72 +13,63 @@ function checkResponse(res) {
   return Promise.reject(`Ошибка: ${res.status}`);
 }
 export function getUserInfo() {
-  return fetch('https://mesto.nomoreparties.co/v1/wff-cohort-36/users/me', {
+  return fetch(`${config.baseUrl}/users/me`, {
     headers: {
-      authorization: '1b90838c-6394-4588-b4b1-865db972e903'
+      authorization: config.headers.authorization
     }
   })
     .then(checkResponse);
 };
 
 export function getInitialCards() {
-  return fetch('https://mesto.nomoreparties.co/v1/wff-cohort-36/cards', {
+  return fetch(`${config.baseUrl}/cards`, {
     headers: {
-      authorization: '1b90838c-6394-4588-b4b1-865db972e903'
+      authorization: config.headers.authorization
     }
   })
     .then(checkResponse);
 };
 
 export function updateUserInfo({ name, about }) {
-  return fetch('https://mesto.nomoreparties.co/v1/wff-cohort-36/users/me', {
+  return fetch(`${config.baseUrl}/users/me`, {
     method: 'PATCH',
-    headers: {
-      authorization: '1b90838c-6394-4588-b4b1-865db972e903',
-      'Content-Type': 'application/json'
-    },
+    headers: config.headers,
     body: JSON.stringify({ name, about })
   })
     .then(checkResponse);
 }
 
 export function addCard({ name, link }) {
-  return fetch('https://mesto.nomoreparties.co/v1/wff-cohort-36/cards', {
+  return fetch(`${config.baseUrl}/cards`, {
     method: 'POST',
-    headers: {
-      authorization: '1b90838c-6394-4588-b4b1-865db972e903',
-      'Content-Type': 'application/json'
-    },
+    headers: config.headers,
     body: JSON.stringify({ name, link })
   })
     .then(checkResponse);
 }
 
 export function updateAvatar({ avatar }) {
-  return fetch('https://mesto.nomoreparties.co/v1/wff-cohort-36/users/me/avatar', {
+  return fetch(`${config.baseUrl}/users/me/avatar`, {
     method: 'PATCH',
-    headers: {
-      authorization: '1b90838c-6394-4588-b4b1-865db972e903',
-      'Content-Type': 'application/json'
-    },
+    headers: config.headers,
     body: JSON.stringify({ avatar })
   })
     .then(checkResponse);
 };
 export function deleteCard(cardId) {
-  return fetch(`https://mesto.nomoreparties.co/v1/wff-cohort-36/cards/${cardId}`, {
+  return fetch(`${config.baseUrl}/cards/${cardId}`, {
     method: 'DELETE',
     headers: {
-      authorization: '1b90838c-6394-4588-b4b1-865db972e903'
+      authorization: config.headers.authorization
     }
   })
     .then(checkResponse);
 }
 export function likeCard(cardId) {
-  return fetch(`https://mesto.nomoreparties.co/v1/wff-cohort-36/cards/likes/${cardId}`, {
+  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
     method: 'PUT',
     headers: {
-      authorization: '1b90838c-6394-4588-b4b1-865db972e903'
+      authorization: config.headers.authorization
     }
   })
     .then(checkResponse);
